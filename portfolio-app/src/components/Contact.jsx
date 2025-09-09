c53e22f7e5d9075effc705d55a69eeb8ab20050b
Uriel Tetteh <urt12597@yahoo.co.uk>
2025-09-09 14:06:47 +0000

    style added to buttons in contact section. Swoosh project added to project section

--- portfolio-app/src/components/Contact.jsx
+++ portfolio-app/src/components/Contact.jsx
@@ -2,18 +2,24 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <section id='contact' className='h-25 rounded-lg flex bg-dark py-1.5 justify-center items-center bg-gray-800 sm:mx-10 md:mx-20 mb-2.5'>
-        <h1 className="w-auto text-white font-bold text-start py-2.5 px-2.5 mx-1.5">Let's connect.</h1>
+    <section id='contact' className='w-auto h-25 rounded-lg flex bg-dark py-1.5 justify-center items-center bg-gray-800 sm:mx-10 md:mx-20 mb-2.5'>
+        <h1 className="w-50 text-white font-bold text-start py-2.5 px-2.5 mx-1.5">Let's connect.</h1>
         <div className="flex text-end gap-x-2.5">
-          <button className="w-1.5/3 cursor-pointer rounded-lg bg-gradient-to-bl from-red-500 to-orange-400 text-white font-bold py-2 px-3.5 transition-all duration-300 hover:bg-gradient-to-tr hover:from-orange-200 hover:to-red-800 hover:scale-105">
-            LinkedIn
-          </button>
-          <button className="w-1.5/3 cursor-pointer rounded-lg bg-gradient-to-bl from-red-500 to-orange-400 text-white font-bold py-2 px-3.5 transition-all duration-300 hover:bg-gradient-to-tr hover:from-orange-200 hover:to-red-800 hover:scale-105">
-            Behance
-          </button>
-          <button className="w-1.5/3 cursor-pointer rounded-lg bg-gradient-to-bl from-red-500 to-orange-400 text-white font-bold py-2 px-3.5 transition-all duration-300 hover:bg-gradient-to-tr hover:from-orange-200 hover:to-red-800 hover:scale-105">
-            Github
-          </button>
+          <a href="https://www.linkedin.com/in/uriel-tetteh-4846b1121/" target="_blank" rel="noopener noreferrer">
+            <button className="w-1.5/3 cursor-pointer rounded-lg bg-gradient-to-bl from-red-500 to-orange-400 text-white font-bold py-2 px-3.5 transition-all duration-300 hover:bg-gradient-to-tr hover:from-orange-200 hover:to-red-800 hover:scale-105">
+              LinkedIn
+            </button>
+          </a>
+          <a href="https://www.behance.net/your-profile" target="_blank" rel="noopener noreferrer">
+            <button className="w-1.5/3 cursor-pointer rounded-lg bg-gradient-to-bl from-red-500 to-orange-400 text-white font-bold py-2 px-3.5 transition-all duration-300 hover:bg-gradient-to-tr hover:from-orange-200 hover:to-red-800 hover:scale-105">
+              Behance
+            </button>
+          </a>
+          <a href="https://github.com/urieltetteh97" target="_blank" rel="noopener noreferrer">
+            <button className="w-1.5/3 cursor-pointer rounded-lg bg-gradient-to-bl from-red-500 to-orange-400 text-white font-bold py-2 px-3.5 transition-all duration-300 hover:bg-gradient-to-tr hover:from-orange-200 hover:to-red-800 hover:scale-105">
+              Github
+            </button>
+          </a>
         </div>
     </section>
   )
--- portfolio-app/src/components/Projects.jsx
+++ portfolio-app/src/components/Projects.jsx
@@ -6,7 +6,7 @@ const Projects = () => {
   return (
     
     
-    <section id='projects' className=" bg-dark text-center justify-center md:py-6.5 sm:py-8.5"> 
+    <section id='projects' className=" bg-dark text-center justify-center md:py-6.5 sm:py-12.5"> 
       <h1 className='font-semibold md:text-5xl sm:text-4xl py-7.5'> Projects</h1>
       <div className="flex flex-col gap-8 items-center">
         <div className="flex flex-col md:flex-row bg-gray-800 rounded-lg shadow-lg hover:scale-110 transition-all ease-in-out duration-200
@@ -17,19 +17,19 @@ const Projects = () => {
           <div className="md:w-2/3 w-full flex flex-col justify-center items-start p-4 text-left">
             <h2 className='text-xl font-bold text-white'>Africam Tech</h2>
             <p className='text-gray-400'>AFricam Tech is a B2B e-commerce website to provide a variety of electronic security services and products to retailers
-               . My responsibilities on this project included product listing through the WooCommerce plug-in and designing of</p>
+               . My responsibilities on this project included product listing through the WooCommerce plug-in and webpage design using Elementor and Worpress themes.</p>
             <a href="https://africamtech.com/" className='text-orange-400 hover:underline mt-2'>View Project</a>
           
           </div>
         </div>
-        <div className="flex flex-col md:flex-row bg-gray-800 rounded-lg shadow-lg hover:scale-110 transition-all ease-in-out duration-200
+        <div className="flex flex-col md:flex-row bg-gray-800 rounded-lg shadow-2xl hover:scale-110 transition-all ease-in-out duration-200
         overflow-hidden w-full max-w-3xl">
           <div className="md:w-3/3 w-full flex justify-center items-center p-4">
             <img src={project1Thumb} alt="Project1 Thumbnail" className='w-5000 h-80 object-cover rounded-2xl ' />
           </div>
           <div className="md:w-2/3 w-full flex flex-col justify-center items-start p-4 text-left">
-            <h2 className='text-xl font-bold text-white'>Africam Tech</h2>
-            <p className='text-gray-400'>A brief description of Project 1 goes here. It highlights the main features and technologies used.</p>
+            <h2 className='text-xl font-bold text-white'>Swoosh Product Delivery Services</h2>
+            <p className='text-gray-400'>This sample project is designed for a company that provides nationwide delivery for various products. This page was built on ReactJS with Tailwind CSS styling and animations and graphics from the Lottie React library. </p>
             <a href="#" className='text-orange-400 hover:underline mt-2'>View Project</a>
           
           </div>
