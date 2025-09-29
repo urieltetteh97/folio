34cb5e3eaaf4560117b83f5b2c045aa8faf6a15c
Uriel Tetteh <urt12597@yahoo.co.uk>
2025-09-29 15:04:21 +0000

    project buttons added

--- portfolio-app/src/components/Projects.jsx
+++ portfolio-app/src/components/Projects.jsx
@@ -61,6 +61,9 @@ const Projects = () => {
             <p className="text-black/80">
               AFricam Tech is a B2B e-commerce website to provide a variety of electronic security services and products to retailers. My responsibilities on this project included product listing through the WooCommerce plug-in and webpage design using Elementor and Worpress themes.
             </p>
+           <button className="rounded-2xl px-4.5 py-3.5 cursor-pointer transform hover:scale-108 transition-all duration-300 ease-in-out bg-gradient-to-r from-teal-500 to-purple-700 w-max mt-4">
+              <span className="text-white font bold text-2xl">View Project</span>
+            </button>
           </motion.div>
         </div>
          <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center px-4 py-7">
@@ -93,6 +96,10 @@ const Projects = () => {
             <p className="text-black/80">
               This sample project is designed for a company that provides nationwide delivery for various products. This page was built on ReactJS with Tailwind CSS styling and animations and graphics from the Lottie React library.
             </p>
+            <button className="rounded-2xl px-4.5 py-3.5 cursor-pointer transform hover:scale-108 transition-all duration-300 ease-in-out bg-gradient-to-r from-teal-500 to-purple-700 w-max mt-4">
+              <span className="text-white font bold text-2xl">View Project</span>
+            </button>
+
           </motion.div>
         </div>
       </div>
